7d24133a30315d7eb35f8603c524a4b977cb67ca
Axel Dalí <a329881@uach.mx>
2021-05-16 13:50:23 -0600

    4) Servidor asíncrono que lea y envíe un archivo.

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const http = require('http');
 const fs = require('fs');
 
 // http => (request, response)
-const data = fs.readFileSync('./WWW/file.txt')
+//const data = fs.readFileSync('./WWW/file.txt')
 
 http.createServer((request, response)=>{
-    response.writeHead(200, {"Content-Type":"text/plain"});
-    response.write(data);
-    response.end();
+    const file = './WWW/file.txt';
+    fs.readFile(file, (err, data)=>{
+        response.writeHead(200, {"Content-Type":"text/plain"});
+        response.write(data);
+        response.end();
+    });
 }).listen(4444);
